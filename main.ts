739c27ba7394a297d6e89b627b64be141d93a5ae
sas1711040 <104405926+sas1711040@users.noreply.github.com>
2022-06-02 10:10:35 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+music.setVolume(255)
 basic.forever(function () {
-	
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+})
+basic.forever(function () {
+    music.playTone(523, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    music.playTone(698, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    basic.pause(100)
 })
