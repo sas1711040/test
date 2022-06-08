bec80d6ce4f7557d8cf3ed1d0b2dfb26c2ee564d
sas1711040 <104405926+sas1711040@users.noreply.github.com>
2022-06-08 10:54:47 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,35 @@
 music.setVolume(255)
 basic.forever(function () {
-    serial.writeLine("" + (sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    )))
-    basic.showNumber(sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    ))
-})
-basic.forever(function () {
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.pause(100)
     music.playTone(523, music.beat(BeatFraction.Half))
     basic.pause(100)
-    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
     basic.pause(100)
-    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
     basic.pause(100)
-    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Whole))
     basic.pause(100)
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.pause(100)
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.pause(2000)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 10) {
+        basic.showString("A")
+    } else {
+        basic.showString("ｂ")
+    }
 })
